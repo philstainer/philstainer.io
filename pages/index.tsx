953ebb39bd0b2ea0f4c52e1f7f09f19e8677de8b
Phil Stainer <phil.stainer@pm.me>
2021-05-04 20:57:24 +0100

    feat: Add project component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,52 @@
 import Link from 'next/link'
 import {Layout} from '@/components/Layout'
+import {Project} from '@/components/Project'
 
 export const Home = (): JSX.Element => {
   return (
     <Layout>
-      <section className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <div className="font-light text-gray-600 dark:text-gray-300 mb-3">
-          Hi, I’m
-        </div>
-        <h1 className="font-bold text-4xl md:text-6xl mb-4 text-gray-800 dark:text-white">
-          Phil Stainer
-        </h1>
-        <h2 className="leading-7 font-light text-gray-600 dark:text-gray-300 mb-5">
-          I’m a full stack developer with a passion for creating and clean code.
-          <br />
-          Currently working at the NHS as a full stack developer.
-        </h2>
-        <p className="prose font-light text-gray-600 dark:text-gray-300">
-          Check out some of my <a href="#projects">projects</a>, or read{' '}
-          <Link href="/blog">articles</Link> I’ve written.
-        </p>
-      </section>
+      <div className="flex flex-col justify-center items-start w-full max-w-3xl mx-auto mb-16">
+        <section className="mb-16 md:mb-28">
+          <div className="font-light text-gray-600 dark:text-gray-300 mb-3">
+            Hi, I’m
+          </div>
+          <h1 className="font-bold text-4xl md:text-6xl mb-4 text-gray-900 dark:text-white">
+            Phil Stainer
+          </h1>
+          <h2 className="leading-7 font-light text-gray-600 dark:text-gray-300 mb-5">
+            I’m a full stack developer with a passion for creating and clean
+            code.
+            <br />
+            Currently working at the NHS as a full stack developer.
+          </h2>
+          <p className="prose dark:prose-dark font-light text-gray-600 dark:text-gray-300">
+            Check out some of my <a href="#projects">projects</a>, or read{' '}
+            <Link href="/blog">articles</Link> I’ve written.
+          </p>
+        </section>
+
+        <section className="w-full">
+          <h3 className="font-bold text-2xl md:text-4xl mb-8 md:mb-12 text-gray-900 dark:text-white">
+            Projects
+          </h3>
+
+          <Project
+            title="philstainer.io"
+            description="My personal site and blog."
+            href="https://philstainer.io"
+            source="https://github.com/PhilStainer/philstainer.io"
+            tags={['next.js', 'tailwind']}
+          />
+
+          <Project
+            title="Easybank Landing Page"
+            description="Landing page built from given design."
+            href="https://easybank-landing-page-philstainer.vercel.app/"
+            source="https://github.com/PhilStainer/easybank-landing-page"
+            tags={['html', 'scss']}
+          />
+        </section>
+      </div>
     </Layout>
   )
 }
